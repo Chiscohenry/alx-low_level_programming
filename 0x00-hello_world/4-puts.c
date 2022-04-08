bd0806371546ff7157a71554e4835ca28a54059f
Chiscohenry <Chiscohenry@gmail.com>
2022-04-08 17:40:06 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * *main - function begins
- * *
- * * Return: function ends
- * */
+ * main -function begins
+ *
+ * Return: function ends
+ */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a mutilingual puzzle");
+	return (0);
 }
-
