5dd5912cc6b13e75f13031b3bd62a7ca42dc78d5
Chiscohenry <Chiscohenry@gmail.com>
2022-04-12 23:49:15 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,18 @@
  */
 void print_alphabet(void)
 {
-		char c = 'a';
-
-			while (c <= 'z')
-					{
-								_putchar(c);
-										c++;
-
-											}
-				_putchar('\n');
-
+	char c;
+	int i = 0;
+	
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putvhar (c);
+			c++;
+		}
+		_putchar('\n')
+			i++;
+	}
 }
