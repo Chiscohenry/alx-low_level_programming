906125c8424ec58fa812016d7f0a1625149520c4
Chiscohenry <Chiscohenry@gmail.com>
2022-05-07 16:03:06 +0000

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,19 +10,19 @@ int main(void)
 	int i = 0;
 	long int a = 0, b = 1, next;
 
-		while (i < 50)
-		{
-			next = a + b;
-			a = b;
-			b = next;
-			printf("%lu", next);
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
 
 		if (i < 49)
 		{
-																								printf(", ");
-																							}
-			i++;
+			printf(", ");
 		}
-			putchar('\n');
-		return (0);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
